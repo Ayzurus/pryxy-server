810aea357d7a3ed260e5698db5c2139c9939c46b
Ayzurus <gmrodg@gmail.com>
2020-11-16 19:40:33 +0000

    Added an http.json as an example file for the configurations

--- server.sh
+++ server.sh
@@ -4,9 +4,11 @@
 # - 1. Host/address for the proxy to listen on
 # - 2. Port for the proxy to listen on
 # - 3. Protocol (only http)
+# - 4. Working directory, where the handler rules are located
 host="localhost"
 port="80"
 proto="http"
-python src/proxyserver.py --host $host --port $port --proto $proto
+directory="$(pwd)/example/"
+python src/proxyserver.py -a $host -p $port -x $proto -d $directory
 echo "press any key to continue..."
 read -rsn1
